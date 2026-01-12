c788dd54c6beffa0cd0ef9b264730a03d84e4f81
Engin Zafer Sönmez <ezafersonmez@gmail.com>
2026-01-12 18:02:50 +0300

    Initial Commit

---
+++ WandWatch/WandWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WandWatchApp.swift
+//  WandWatch
+//
+//  Created by Engin Zafer Sönmez on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WandWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
